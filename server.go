cc13d94e7dc8ab388f6a93d1f54601905bdaa788
Clark Green <clark_green@byu.edu>
2017-08-04 20:01:14 -0600

    The claw is our master

---
+++ server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/byuoitav/authmiddleware"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func main() {
+
+	port := ":5000"
+	router := echo.New()
+	router.Pre(middleware.RemoveTrailingSlash())
+	router.Use(middleware.CORS())
+
+	secure := router.Group("", echo.WrapMiddleware(authmiddleware.Authenticate))
+
+	secure.GET("/health", health)
+
+	server := http.Server{
+		Addr:           port,
+		MaxHeaderBytes: 1024 * 10,
+	}
+
+	router.StartServer(&server)
+
+}
+
+func health(context echo.Context) error {
+
+	return context.JSON(http.StatusOK, os.Getenv("HEALTH_MESSAGE"))
+
+}
